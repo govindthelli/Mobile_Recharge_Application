ad5b56cc8a360acad3ed7135afd9b1defd0ce6d5
Thelli Govinda Rao <147299435+govindthelli@users.noreply.github.com>
2025-11-14 09:43:30 +0530

    Update SubscriberController.java

--- backend/src/main/java/com/example/demo/controller/SubscriberController.java
+++ backend/src/main/java/com/example/demo/controller/SubscriberController.java
@@ -14,12 +14,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
-@CrossOrigin(origins = {
-    "http://localhost:3000",
-    "https://mobile-recharge-frontend.onrender.com",
-    "http://frontend:3000",
-    "http://54.165.238.130:3000"
-})
+// @CrossOrigin(origins = {
+//     "http://localhost:3000",
+//     "https://mobile-recharge-frontend.onrender.com",
+//     "http://frontend:3000",
+//     "http://54.165.238.130:3000"
+// })
 
 public class SubscriberController {
     @Autowired
