19c6078fb5c757f1dffe0b50f3d62987dcc186cc
Thelli Govinda Rao <147299435+govindthelli@users.noreply.github.com>
2025-12-19 17:43:39 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL + "/api";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 // Create Axios instance
 const api = axios.create({
